0186012901940ae0d9010deeb2413e40359c7b58
Aren1219 <arenwang1219@gmail>
2022-12-06 17:14:33 +0000

    detail page almost done

--- app/src/main/java/com/example/faurecia_aptoidetechnicalchallenge/ui/DetailPage.kt
+++ app/src/main/java/com/example/faurecia_aptoidetechnicalchallenge/ui/DetailPage.kt
@@ -1,6 +1,5 @@
 package com.example.faurecia_aptoidetechnicalchallenge.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,9 +8,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.faurecia_aptoidetechnicalchallenge.model.App
 import com.example.faurecia_aptoidetechnicalchallenge.ui.destinations.MainPageDestination
@@ -28,6 +31,7 @@ fun DetailPage(app: App, navigator: DestinationsNavigator) {
             .verticalScroll(rememberScrollState(), true)
             .fillMaxSize()
     ) {
+
         Box() {
             GlideImage(app.graphic)
             Button(
@@ -35,13 +39,15 @@ fun DetailPage(app: App, navigator: DestinationsNavigator) {
                     .padding(8.dp)
                     .size(48.dp),
                 onClick = { navigator.navigate(MainPageDestination) },
-                contentPadding = PaddingValues(0.dp)
+                contentPadding = PaddingValues(0.dp),
+                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary.copy(0.6f))
             ) {
                 Icon(
                     Icons.Default.ArrowBack, "navigate up",
                 )
             }
         }
+
         Row(
             modifier = Modifier
                 .padding(12.dp)
@@ -56,7 +62,7 @@ fun DetailPage(app: App, navigator: DestinationsNavigator) {
             )
             Column(
                 Modifier
-                    .padding(horizontal = 18.dp)
+                    .padding(start = 18.dp)
                     .fillMaxSize(),
                 Arrangement.SpaceBetween
             ) {
@@ -64,15 +70,54 @@ fun DetailPage(app: App, navigator: DestinationsNavigator) {
                     Text(
                         text = app.name,
                         style = MaterialTheme.typography.titleLarge,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
                     )
                     Text(text = app.storeName)
                 }
                 Button(onClick = { /*TODO*/ }) {
-                    Text(text = "DOWNLOAD")
+                    Text(text = "INSTALL")
                 }
             }
         }
-        Divider(color = Color.Gray)
-    }
 
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 0.dp, 0.dp, 12.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.semantics(mergeDescendants = true) { }
+            ) {
+                AppRating(rating = app.rating, color = Color.Black)
+                Text(text = "App rating", modifier = Modifier.clearAndSetSemantics { })
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.semantics(mergeDescendants = true) { }
+            ) {
+                Text(text = "${app.downloads}")
+                Text(text = "downloads")
+            }
+
+        }
+
+        Divider(color = Color.Gray)
+
+        Column(
+            modifier = Modifier
+                .padding(12.dp)
+                .semantics(mergeDescendants = true) { }
+        ) {
+            Text(text = "date added: ${app.added.formatDate()}")
+            Text(text = "last updated: ${app.updated.formatDate()}")
+        }
+
+        Text(text = "App description: ...", modifier = Modifier.padding(horizontal = 12.dp))
+    }
 }
+
+fun String.formatDate() = this.take(10)
--- app/src/test/java/com/example/faurecia_aptoidetechnicalchallenge/ExampleUnitTest.kt
+++ app/src/test/java/com/example/faurecia_aptoidetechnicalchallenge/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.faurecia_aptoidetechnicalchallenge
 
+import app.cash.turbine.test
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,58 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val flow = flow {
+        emit(0)
+        emit(1)
+        emit(2)
+        emit(3)
+    }
+
+    private lateinit var stateFlow: MutableStateFlow<Int>
+    private suspend fun updateStateFlow() {
+        stateFlow.emit(1)
+        stateFlow.emit(2)
+        stateFlow.emit(3)
+    }
+
+    @Before
+    fun setup() {
+        stateFlow = MutableStateFlow(0)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test flow`() = runBlocking {
+        assertEquals(0, flow.first())
+    }
+
+    @Test
+    fun `test stateflow`() = runBlocking {
+        assertEquals(0, stateFlow.first())
+        updateStateFlow()
+        assertEquals(3, stateFlow.first())
+    }
+
+    //turbine
+    @Test
+    fun `test flow with turbine`() = runBlocking {
+        flow.test {
+            assertEquals(0, awaitItem())
+            assertEquals(1, awaitItem())
+            assertEquals(2, awaitItem())
+            assertEquals(3, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `test stateflow with turbine`() = runBlocking {
+        stateFlow.test {
+            updateStateFlow()
+            assertEquals(0, awaitItem())
+            assertEquals(1, awaitItem())
+            assertEquals(2, awaitItem())
+            assertEquals(3, awaitItem())
+        }
     }
 }
